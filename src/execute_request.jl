d13ff197d123f21dd6158829a5897a01652bad36
WooKyoung Noh <wookay.noh@gmail.com>
2016-07-24 12:24:26 +0900

    a

---
+++ src/execute_request.jl
@@ -0,0 +1,258 @@
+# Handers for execute_request and related messages, which are
+# the core of the Jupyter protocol: execution of Julia code and
+# returning results.
+
+if VERSION >= v"0.4.0-dev+3844"
+    import Base.Libc: flush_cstdio
+end
+
+using Compat
+import Compat.String
+
+const text_plain = MIME("text/plain")
+const image_svg = MIME("image/svg+xml")
+const image_png = MIME("image/png")
+const image_jpeg = MIME("image/jpeg")
+const text_markdown = MIME("text/markdown")
+const text_html = MIME("text/html")
+const text_latex = MIME("text/latex") # Jupyter expects this
+const text_latex2 = MIME("application/x-latex") # but this is more standard?
+
+# return a AbstractString=>Any dictionary to attach as metadata
+# in Jupyter display_data and pyout messages
+metadata(x) = Dict()
+
+# return a AbstractString=>AbstractString dictionary of mimetype=>data
+# for passing to Jupyter display_data and execute_result messages.
+function display_dict(x)
+    data = @compat Dict{String,String}("text/plain" => stringmime(text_plain, x))
+    if mimewritable(image_svg, x)
+        data[string(image_svg)] = stringmime(image_svg, x)
+    end
+    if mimewritable(image_png, x)
+        data[string(image_png)] = stringmime(image_png, x)
+    elseif mimewritable(image_jpeg, x) # don't send jpeg if we have png
+        data[string(image_jpeg)] = stringmime(image_jpeg, x)
+    end
+    if mimewritable(text_markdown, x)
+        data[string(text_markdown)] = stringmime(text_markdown, x)
+    elseif mimewritable(text_html, x)
+        data[string(text_html)] = stringmime(text_html, x)
+    end
+    if mimewritable(text_latex, x)
+        data[string(text_latex)] = stringmime(text_latex, x)
+    elseif mimewritable(text_latex2, x)
+        data[string(text_latex)] = stringmime(text_latex2, x)
+    end
+    return data
+end
+
+# queue of objects to display at end of cell execution
+const displayqueue = Any[]
+
+# remove x from the display queue
+function undisplay(x)
+    i = findfirst(displayqueue, x)
+    if i > 0
+        splice!(displayqueue, i)
+    end
+    return x
+end
+
+#######################################################################
+
+if v"0.4.0-dev+6438" <= VERSION < v"0.4.0-dev+6492" # julia PR #12250
+    function show_bt(io::IO, top_func::Symbol, t, set)
+        process_entry(lastname, lastfile, lastline, n) = Base.show_trace_entry(io, lastname, lastfile, lastline, n)
+        Base.process_backtrace(process_entry, top_func, t, set)
+    end
+else
+    show_bt(io::IO, top_func::Symbol, t, set) =
+        Base.show_backtrace(io, top_func, t, set)
+end
+
+# return the content of a pyerr message for exception e
+function error_content(e, bt=catch_backtrace(); backtrace_top::Symbol=:execute_request_0x535c5df2, msg::AbstractString="")
+    tb = map(x->convert(Compat.UTF8String, x), @compat(split(sprint(show_bt,
+                                        backtrace_top,
+                                        bt, 1:typemax(Int)),
+                                 "\n", keep=true)))
+    if !isempty(tb) && ismatch(r"^\s*in\s+include_string\s+", tb[end])
+        pop!(tb) # don't include include_string in backtrace
+    end
+    ename = string(typeof(e))
+    evalue = try
+        sprint(VERSION < v"0.4.0-dev+5252" ? (io, e, bt) -> showerror(io, e) :
+               (io, e, bt) -> showerror(io, e, bt, backtrace=false), e, bt)
+    catch
+        "SYSTEM: show(lasterr) caused an error"
+    end
+    unshift!(tb, evalue) # fperez says this needs to be in traceback too
+    if !isempty(msg)
+        unshift!(tb, msg)
+    end
+    @compat Dict("ename" => ename, "evalue" => evalue,
+                 "traceback" => tb)
+end
+
+#######################################################################
+# Similar to the ipython kernel, we provide a mechanism by
+# which modules can register thunk functions to be called after
+# executing an input cell, e.g. to "close" the current plot in Pylab.
+# Modules should only use these if isdefined(Main, IJulia) is true.
+
+const postexecute_hooks = Function[]
+push_postexecute_hook(f::Function) = push!(postexecute_hooks, f)
+pop_postexecute_hook(f::Function) = splice!(postexecute_hooks, findfirst(postexecute_hooks, f))
+
+const preexecute_hooks = Function[]
+push_preexecute_hook(f::Function) = push!(preexecute_hooks, f)
+pop_preexecute_hook(f::Function) = splice!(preexecute_hooks, findfirst(pretexecute_hooks, f))
+
+# similar, but called after an error (e.g. to reset plotting state)
+const posterror_hooks = Function[]
+push_posterror_hook(f::Function) = push!(posterror_hooks, f)
+pop_posterror_hook(f::Function) = splice!(posterror_hooks, findfirst(posterror_hooks, f))
+
+#######################################################################
+
+# global variable so that display can be done in the correct Msg context
+execute_msg = Msg(["julia"], @compat(Dict("username"=>"julia", "session"=>"????")), Dict())
+
+if VERSION >= v"0.4.0-dev+1853"
+    # in Julia commit edbfd4053ccd2970789931ad56dc336c8dd7f029,
+    # repl_cmd(cmd) was replaced by repl_cmd(cmd, out); just add the old method
+    Base.repl_cmd(cmd) = Base.repl_cmd(cmd, STDOUT)
+end
+
+function helpcode(code::AbstractString)
+    if VERSION < v"0.4.0-dev+2891" # old Base.@help macro
+        return "Base.@help " * code
+    else # new Base.Docs.@repl macro from julia@08663d4bb05c5b8805a57f46f4feacb07c7f2564
+        code_ = strip(code)
+        # as in base/REPL.jl, special-case keywords so that they parse
+        @compat if !haskey(Docs.keywords, Symbol(code_))
+            return "Base.Docs.@repl $code_"
+        elseif VERSION < v"0.5.0-dev+3831"
+            return "eval(:(Base.Docs.@repl \$(symbol(\"$code_\"))))"
+        else
+            return "eval(:(Base.Docs.@repl \$(Symbol(\"$code_\"))))"
+        end
+    end
+end
+
+# note: 0x535c5df2 is a random integer to make name collisions in
+# backtrace analysis less likely.
+function execute_request_0x535c5df2(socket, msg)
+    code = msg.content["code"]
+    @vprintln("EXECUTING ", code)
+    global execute_msg = msg
+    global _n, In, Out, ans
+    silent = msg.content["silent"]
+    store_history = get(msg.content, "store_history", !silent)
+
+    if !silent
+        _n += 1
+        send_ipython(publish,
+                     msg_pub(msg, "execute_input",
+                             @compat Dict("execution_count" => _n,
+                                          "code" => code)))
+    end
+
+    silent = silent || ismatch(r";\s*$", code)
+    if store_history
+        In[_n] = code
+    end
+
+    # "; ..." cells are interpreted as shell commands for run
+    code = replace(code, r"^\s*;.*$",
+                   m -> string(replace(m, r"^\s*;", "Base.repl_cmd(`"),
+                               "`)"), 0)
+
+    # a cell beginning with "? ..." is interpreted as a help request
+    hcode = replace(code, r"^\s*\?", "")
+    if hcode != code
+        code = helpcode(hcode)
+    end
+
+    try
+        for hook in preexecute_hooks
+            hook()
+        end
+
+        #run the code!
+        ans = result = include_string(code, "In[$_n]")
+
+        if silent
+            result = nothing
+        elseif result != nothing
+            if store_history
+                if result != Out # workaround for Julia #3066
+                    Out[_n] = result
+                end
+            end
+        end
+
+        user_expressions = Dict()
+        for (v,ex) in msg.content["user_expressions"]
+            user_expressions[v] = eval(Main,parse(ex))
+        end
+
+        for hook in postexecute_hooks
+            hook()
+        end
+
+        # flush pending stdio
+        flush_all()
+
+        undisplay(result) # dequeue if needed, since we display result in pyout
+        display() # flush pending display requests
+
+        if result != nothing
+            # Work around for Julia issue #265 (see # #7884 for context)
+            # We have to explicitly invoke the correct metadata method.
+            result_metadata = invoke(metadata, (typeof(result),), result)
+            send_ipython(publish,
+                         msg_pub(msg, "execute_result",
+                                 @compat Dict("execution_count" => _n,
+                                              "metadata" => result_metadata,
+                                              "data" => display_dict(result))))
+
+        end
+        send_ipython(requests,
+                     msg_reply(msg, "execute_reply",
+                               @compat Dict("status" => "ok",
+                                            "payload" => "", # TODO: remove (see #325)
+                                            "execution_count" => _n,
+                                            "user_expressions" => user_expressions)))
+    catch e
+        bt = catch_backtrace()
+        try
+            # flush pending stdio
+            flush_all()
+            for hook in posterror_hooks
+                hook()
+            end
+        catch
+        end
+        empty!(displayqueue) # discard pending display requests on an error
+        content = error_content(e,bt)
+        send_ipython(publish, msg_pub(msg, "error", content))
+        content["status"] = "error"
+        content["execution_count"] = _n
+        send_ipython(requests, msg_reply(msg, "execute_reply", content))
+    end
+end
+
+#######################################################################
+
+# The user can call IJulia.clear_output() to clear visible output from the
+# front end, useful for simple animations.  Using wait=true clears the
+# output only when new output is available, for minimal flickering.
+function clear_output(wait=false)
+    # flush pending stdio
+    flush_all()
+    empty!(displayqueue) # discard pending display requests
+    send_ipython(publish, msg_reply(execute_msg::Msg, "clear_output",
+                                    @compat Dict("wait" => wait)))
+end
---
+++ src/msg.jl
@@ -0,0 +1,114 @@
+import Base.show
+
+export Msg, msg_pub, msg_reply, send_status, send_ipython
+
+# IPython message structure
+type Msg
+    idents::Vector{AbstractString}
+    header::Dict
+    content::Dict
+    parent_header::Dict
+    metadata::Dict
+    function Msg(idents, header::Dict, content::Dict,
+                 parent_header=Dict{AbstractString,Any}(), metadata=Dict{AbstractString,Any}())
+        new(idents,header,content,parent_header,metadata)
+    end
+end
+
+# PUB/broadcast messages use the msg_type as the ident, except for
+# stream messages which use the stream name (e.g. "stdout").
+# [According to minrk, "this isn't well defined, or even really part
+# of the spec yet" and is in practice currently ignored since "all
+# subscribers currently subscribe to all topics".]
+msg_pub(m::Msg, msg_type, content, metadata=Dict{AbstractString,Any}()) =
+  Msg([ msg_type == "stream" ? content["name"] : msg_type ],
+      @compat(Dict("msg_id" => uuid4(),
+                   "username" => m.header["username"],
+                   "session" => m.header["session"],
+                   "msg_type" => msg_type,
+                   "version" => "5.0")),
+      content, m.header, metadata)
+
+msg_reply(m::Msg, msg_type, content, metadata=Dict{AbstractString,Any}()) =
+  Msg(m.idents,
+      @compat(Dict("msg_id" => uuid4(),
+                   "username" => m.header["username"],
+                   "session" => m.header["session"],
+                   "msg_type" => msg_type,
+                   "version" => "5.0")),
+      content, m.header, metadata)
+
+function show(io::IO, msg::Msg)
+    print(io, "IPython Msg [ idents ")
+    print(io, join(msg.idents, ", "))
+    print(io, " ] {\n  parent_header = $(msg.parent_header),\n  header = $(msg.header),\n  metadata = $(msg.metadata),\n  content = $(msg.content)\n}")
+end
+
+function send_ipython(socket, m::Msg)
+    @vprintln("SENDING $m")
+    for i in m.idents
+        send(socket, i, SNDMORE)
+    end
+    send(socket, "<IDS|MSG>", SNDMORE)
+    header = json(m.header)
+    parent_header = json(m.parent_header)
+    metadata = json(m.metadata)
+    content = json(m.content)
+    send(socket, hmac(header, parent_header, metadata, content), SNDMORE)
+    send(socket, header, SNDMORE)
+    send(socket, parent_header, SNDMORE)
+    send(socket, metadata, SNDMORE)
+    send(socket, content)
+end
+
+function recv_ipython(socket)
+    msg = recv(socket)
+    idents = AbstractString[]
+    # unsafe_string for ZMQ.jl has patched
+    s = bytestring(msg)
+    @vprintln("got msg part $s")
+    while s != "<IDS|MSG>"
+        push!(idents, s)
+        msg = recv(socket)
+        s = bytestring(msg)
+        @vprintln("got msg part $s")
+    end
+    signature = bytestring(recv(socket))
+    request = Dict{AbstractString,Any}()
+    header = bytestring(recv(socket))
+    parent_header = bytestring(recv(socket))
+    metadata = bytestring(recv(socket))
+    content = bytestring(recv(socket))
+    if signature != hmac(header, parent_header, metadata, content)
+        error("Invalid HMAC signature") # What should we do here?
+    end
+    m = Msg(idents, JSON.parse(header), JSON.parse(content), JSON.parse(parent_header), JSON.parse(metadata))
+    @vprintln("RECEIVED $m")
+    return m
+end
+
+function send_status(state::AbstractString, parent_header=nothing)
+    if parent_header == nothing
+        msg = Msg(
+                  [ "status" ],
+                  @compat(Dict("msg_id" => uuid4(),
+                               "username" => "jlkernel",
+                               "session" => execute_msg.header["session"],
+                               "msg_type" => "status",
+                               "version" => "5.0")),
+                  @compat(Dict("execution_state" => state))
+        )
+    else
+        msg = Msg(
+                  [ "status" ],
+                  @compat(Dict("msg_id" => uuid4(),
+                               "username" => "jlkernel",
+                               "session" => execute_msg.header["session"],
+                               "msg_type" => "status",
+                               "version" => "5.0")),
+                  @compat(Dict("execution_state" => state)),
+                  parent_header
+        )
+    end
+    send_ipython(publish, msg)
+end
